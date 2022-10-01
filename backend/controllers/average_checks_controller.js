0bdc540dff11f834dceb529b2b643c14d4814ee1
Joel Acquah <joelaj06@gmail.com>
2022-10-01 13:43:11 +0000

    Weekly average check out implemented

--- backend/controllers/average_checks_controller.js
+++ backend/controllers/average_checks_controller.js
@@ -73,7 +73,63 @@ const averageCheckInOfTheWeek = asyncHandler(async (req, res) => {
 });
 
 const averageCheckOutOfTheWeek = asyncHandler(async (req, res) => {
-  res.send("weekly average check out");
+  let dates = [];
+  dates = getAllPreviousDatesByDays(7);
+
+  let avgTimes = ["00:00","00:00","00:00","00:00","00:00","00:00","00:00"];
+
+  const attendanceDates = await AttendanceDate.find({
+    createdAt: { $gte : getLastDate(now, 7)}
+  });
+
+  if (attendanceDates) {
+    const checkOutDates = attendanceDates.map(({check_out, createdAt}) => {
+      let date = createdAt.toISOString().split("T")[0];
+      let checkOut = changeToSeconds(check_out);
+      return {checkOut, date}
+    });
+
+    const keys = ["date"];
+    count = {};
+    const results = checkOutDates.reduce(function (r, o) {
+      var key = keys
+        .map(function (k) {
+          return o[k];
+        })
+        .join("|");
+      if (!count[key]) {
+        count[key] = {
+          sum: +o.checkOut,
+          checkOutDates: JSON.parse(JSON.stringify(o)),
+        };
+        count[key].checkOutDates.count = 1;
+        r.push(count[key].checkOutDates);
+      } else {
+        count[key].sum += +o.checkOut;
+        count[key].checkOutDates.checkOut = (
+          count[key].sum / ++count[key].checkOutDates.count
+        );
+      }
+      return r;
+    }, []);
+
+    const averageTimes = results.map(({checkOut, date}) => {
+      let avgCheckOutTime = convertToHM(checkOut);
+      return {avgCheckOutTime, date};
+    });
+
+    averageTimes.forEach((averageTime) =>{
+      dates.forEach((date) => {
+        if(date === averageTime.date){
+          let index = dates.indexOf(date);
+          avgTimes[index] = averageTime.avgCheckOutTime;
+        }
+      });
+    });
+    res.status(200).json({dates,avgTimes});
+  }else{
+    throw new Error('Failed to fetch dates')
+  }
 });
 
 module.exports = {
