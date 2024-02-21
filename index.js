34e80c6261d8161e10c1d3eabb91c1eff5cf5e1c
Joel Acquah <jacquah@appsolinfosystems.com>
2024-02-21 19:46:13 +0000

    CORS enable

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-const express = require('express');
-const {connectToDatabase} = require('./backend/config/conn.js');
-const users = require('./backend/routes/users');
-const {errorHandler }= require('./backend/middleware/error_middleware.js');
-const attendance_dates = require('./backend/routes/attendance_dates');
-const roles = require('./backend/routes/roles');
-const teams = require('./backend/routes/teams')
-const organization = require('./backend/routes/organization');
-const activeUsersOfTheWeek = require('./backend/routes/active_users_of_the_week');
-const averageChecksOfTheWeek = require('./backend/routes/average_checks_of_the_week');
-const leaves = require('./backend/routes/leaves');
-const leavesOfTheWeek = require('./backend/routes/leaves_of_the_week');
-
-
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase } = require("./backend/config/conn.js");
+const users = require("./backend/routes/users");
+const { errorHandler } = require("./backend/middleware/error_middleware.js");
+const attendance_dates = require("./backend/routes/attendance_dates");
+const roles = require("./backend/routes/roles");
+const teams = require("./backend/routes/teams");
+const organization = require("./backend/routes/organization");
+const activeUsersOfTheWeek = require("./backend/routes/active_users_of_the_week");
+const averageChecksOfTheWeek = require("./backend/routes/average_checks_of_the_week");
+const leaves = require("./backend/routes/leaves");
+const leavesOfTheWeek = require("./backend/routes/leaves_of_the_week");
 
 const app = express();
 
 connectToDatabase();
 
-
 app.use(express.json());
+
+//enable CORS for all routes
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-
-
-app.use('/api/users', users);
-app.use('/api/attendance_dates', attendance_dates);
-app.use('/api/roles',roles);
-app.use('/api/teams',teams);
-app.use('/api/organization',organization);
-app.use('/api/activeUsersOfTheWeek', activeUsersOfTheWeek);
-app.use('/api/averageChecksOfTheWeek', averageChecksOfTheWeek);
-app.use('/api/leaves', leaves);
-app.use('/api/leavesOfTheWeek', leavesOfTheWeek);
+app.use("/api/users", users);
+app.use("/api/attendance_dates", attendance_dates);
+app.use("/api/roles", roles);
+app.use("/api/teams", teams);
+app.use("/api/organization", organization);
+app.use("/api/activeUsersOfTheWeek", activeUsersOfTheWeek);
+app.use("/api/averageChecksOfTheWeek", averageChecksOfTheWeek);
+app.use("/api/leaves", leaves);
+app.use("/api/leavesOfTheWeek", leavesOfTheWeek);
 
 app.use(errorHandler);
 
-app.listen(port, () =>{
-    console.log(`Sever started on port ${port}`);
-    console.log(`http://localhost:${port}`);
-})
-
-
-
-
+app.listen(port, () => {
+  console.log(`Sever started on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
