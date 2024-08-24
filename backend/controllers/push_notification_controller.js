7c16df72c3f87d96597339b466ad5e5815045dc4
Joel Acquah <jacquah@appsolinfosystems.com>
2024-08-24 02:49:03 +0000

    Removed key file

--- backend/controllers/push_notification_controller.js
+++ backend/controllers/push_notification_controller.js
@@ -1,25 +1,24 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../push_notification_key.json");
+const serviceAccount = require("../../private/push_notification_key.json");
 
- admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
-
-///This function sends a notification to the client specific device using the [token] as an identifier for the users device 
-const sendPushNotification = (payload) =>{
-    try {
-       admin.messaging().send(payload).then((response) =>{
+///This function sends a notification to the client specific device using the [token] as an identifier for the users device
+const sendPushNotification = (payload) => {
+  try {
+    admin
+      .messaging()
+      .send(payload)
+      .then((response) => {
         console.log(response);
-       }).catch((err) => console.log(err));
-    } catch (error) {
-        console.log(error);
-    }
-}
+      })
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-module.exports.sendPushNotification = sendPushNotification
-
-
-
+module.exports.sendPushNotification = sendPushNotification;
